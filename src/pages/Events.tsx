34586de560ab23fb9e8867842f677176aef2a8b1
Daniel Nicolau <2232623@my.ipleiria.pt>
2025-11-12 21:03:00 +0000

    updated year MDI MSP

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -3,7 +3,7 @@ import WorkshopLatex from './events/2024/WorkshopLatex';
 import WorkshopPytorch from './events/2024/WorkshopPytorch';
 import WorkshopMicrocontroladores from './events/2024/WorkshopMicroControladores';
 import MdiLLMS from './events/2025/MdiLLMS';
-import MDImsp from './events/2025/MDImsp.tsx';
+import MDImsp from './events/2024/MDImsp.tsx';
 import DlAutonomous from './events/2025/DlAutonomousDriving';
 import SectionConclusionEvents from './events/SectionConclusionEvents';
 
